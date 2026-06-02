cf6818aeef0882a5449222b90520379787081605
itsmekai0413-creator <itsmekai0413@gmail.com>
2026-06-02 11:49:38 +0530

    Implement three problems in 55-karan-gabhale.py
    Added three problems: counting, even/odd check, and divisibility check.

--- lab/55-karan-gabhale.py
+++ lab/55-karan-gabhale.py
@@ -1,6 +1,38 @@
+#problem 1
 x=0
 y=1
 while x<10:
   x=x+1
   total=total+1
   print(total)
+
+#problem 2
+def even_odd(number):
+    if number % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+a=4
+b=9
+c=19
+
+print("a",even_odd(a))
+print("b",even_odd(b))
+print("c",even_odd(c))
+
+# problem 3
+
+def div_3(number):
+    if number % 3 ==0:
+        return("divisible by 3")
+    else:
+        return("not divisible by 3")
+a=4
+b=9
+c=19
+
+print("a",div_3(a))
+print("b",div_3(b))
+print("c",div_3(c))
+            
+        
