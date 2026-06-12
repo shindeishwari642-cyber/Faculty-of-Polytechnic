1031c79d7f4ac35b9b809e174bf131e0bf4d4a14
shindeishwari642-cyber <shindeishwari642@gmail.com>
2026-06-12 11:53:24 +0530

    Update Solution6.py

--- students/ishwari-shinde/Solution6.py
+++ students/ishwari-shinde/Solution6.py
@@ -7,3 +7,20 @@ lang = input("Favourite language: ")
 years = int(input("Coding years: "))
 
 print(f"Name: {name}\nAge: {age}\nCity: {city}\nLanguage: {lang}\nCoding Years: {years}")
+
+#problem 2
+#whole numbers 
+while True:
+    name = input("Name: ")
+    if name:
+        break
+    print("Name can't be empty — try again.")
+
+while True:
+    age = input("Age: ")
+    if age.isdigit():
+        age = int(age)
+        break
+    print("Please type a whole number.")
+
+print(name, age)
