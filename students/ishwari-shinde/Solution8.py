9594c685c62b2be9dbde7a277954de8b37d6fd65
shindeishwari642-cyber <shindeishwari642@gmail.com>
2026-06-15 11:34:30 +0530

    Update Solution8.py

--- students/ishwari-shinde/Solution8.py
+++ students/ishwari-shinde/Solution8.py
@@ -15,3 +15,27 @@ def countdown (n):
 countdown (5)
 
 
+#problem 3
+#play game
+
+
+SECRET = 10
+
+def play_game():
+    guesses = 0
+
+    while True:
+        guess = int(input("Enter your guess: "))
+        guesses =+1
+
+        if guess > SECRET:
+            print("Too high!")
+        elif guess < SECRET:
+            print("Too low!")
+        else:
+            print(f"Correct! You got it in {guesses} tries.")
+            break
+
+play_game()
+
+
